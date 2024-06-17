453395e69049532be58b53d871d9403894055168
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-06-17 17:27:36 +0300

    Completed Header and add tailwind

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+
 import Header from "./components/Header/header";
+import Example from "./components/Card/card";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Example />
       </div>
     </Router>
   );
