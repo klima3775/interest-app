2dc8097e2facc3e2a711d7f3e40d96e03d2afa34
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-06-17 17:28:19 +0300

    Fix problem

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Header/header";
-import Example from "./components/Card/card";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Example />
       </div>
     </Router>
   );
