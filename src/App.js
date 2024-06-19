dde8d33a9bc1553595affa28aa76a283bd71ab0b
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-06-20 01:08:22 +0300

    Add ticker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Slider from "./components/Slider/slider";
+import Ticker from "./components/Ticker/ticker";
 
 import Header from "./components/Header/header";
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <Header />
+        <Ticker />
         <Slider />
       </div>
     </Router>
